16d1f80f9753c04f6c9c9ece82aa4824e89f5def
PrestigeGiraffe <johnsonyep@hotmail.com>
2024-06-05 22:18:19 -0400

    Wrap start and end coordinates in Point2D

--- Space Race/src/Projectile.java
+++ Space Race/src/Projectile.java
@@ -2,26 +2,25 @@
  * Purpose: Moves the projectile
  */
 
+import javafx.geometry.Point2D;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class Projectile extends Rectangle {
-    private double endX, endY;
+    Point2D startPoint, endPoint;
     private int speed;
 
     // Creates a new projectile and stores the values passed into the constructor as class variables
     Projectile(double startX, double startY, double endX, double endY, int size, int speed) {
         super(startX, startY, size, size);
-        this.endX = endX;
-        this.endY = endY;
+        this.startPoint = new Point2D(startX, startY);
+        this.endPoint = new Point2D(endX, endY);
         this.speed = speed;
     }
 
     public void move() {
-        double xDist = endX - this.getX();
-        double yDist = endY - this.getY();
-
-
+        double xDist = startPoint.getX() - this.getX();
+        double yDist = startPoint.getY() - this.getY();
         
     if (xDist > 0 && yDist > 0) 
         {
