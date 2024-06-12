dcd0ca552039f7186dd1bc331f05bce7d78d16f2
PrestigeGiraffe <johnsonyep@hotmail.com>
2024-06-11 21:33:29 -0400

    Add optional constructor to Projectile Add constructor parameter for the projectile's color Implement projectile movement

--- Space Race/src/Projectile.java
+++ Space Race/src/Projectile.java
@@ -3,15 +3,22 @@
  * Author: Johnson Yep
  */
 
+import java.util.List;
+
+import javafx.scene.Scene;
+import javafx.scene.layout.Pane;
 import javafx.scene.paint.Paint;
 import javafx.scene.shape.Rectangle;
 
 public class Projectile extends Rectangle {
     private double speedX, speedY;
+    private boolean canHit = true;
+
+    Projectile() {}
 
     // Creates a new projectile and stores the values passed into the constructor as
     // class variables
-    Projectile(double startX, double startY, double endX, double endY, int size, int speed) {
+    Projectile(double startX, double startY, double endX, double endY, int size, int speed, Paint color) {
         super(startX, startY, size, size);
         
         // Get hypotenuse of the x and y values of the end point to calculate the right speed for the x value to be changed by so they can reach the same point at the same time if the x and y end points are different
@@ -21,11 +28,34 @@ public class Projectile extends Rectangle {
         double hyp = Math.sqrt(Math.pow(totalX, 2) + Math.pow(totalY, 2)); // ratio for speed calculation
         speedX = (totalX / hyp) * speed;
         speedY = (totalY / hyp) * speed;
-        this.setFill(Paint.valueOf("Yellow"));
+        this.setFill(color);
     }
 
     public void move() {
         this.setX(this.getX() + speedX);
         this.setY(this.getY() + speedY);
+    }
+
+    // Loops through all the alien projectiles on the screen and updates their position
+    public void updateProjectiles(Scene scene, Pane root, List<Projectile> alienProjectiles) {
+        for (int i = 0; i < alienProjectiles.size(); i++) { // used a regular for loop instead of an enhanced for loop because it caused ConcurrentModificationExceptions when projectiles were being removed
+            Projectile projectile = alienProjectiles.get(i);
+            projectile.move();
+
+            // Deletes the projectile from the layout and the list once it goes off the screen
+            if (projectile.getX() > scene.getWidth() || projectile.getY() > scene.getHeight() || projectile.getX() < 0 || projectile.getY() < 0) {
+                root.getChildren().remove(projectile);
+                alienProjectiles.remove(projectile);
+            }
+        }
+    }
+
+    // GETTER AND SETTER
+    public boolean getCanHit() {
+        return canHit;
+    }
+
+    public void setCanHit(boolean canHit) {
+        this.canHit = canHit;
     }
 }
